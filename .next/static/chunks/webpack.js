bbd286016b0f6f6270bab2fcaea254ce3599f969
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-01-24 20:26:57 +0900

    add : Link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f63b9fc364b59af028a2";
+/******/ 	var hotCurrentHash = "2e323b3619986e1edfc5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
