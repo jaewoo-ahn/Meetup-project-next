ffa314852119264bd3bcc231ce437fa427b8d8d5
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-01-25 22:56:59 +0900

    Add : MeetupDetail Components

--- pages/[meetupID]/index.js
+++ pages/[meetupID]/index.js
@@ -1,16 +1,13 @@
-import {Fragment} from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
   return (
-    <Fragment>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/5/58/Milan.Proper.Wikipedia.Image.png"
-        alt="A First Meetup"
-      />
-      <h1>A First Meetup</h1>
-      <address>Some Street 5, Some City</address>
-      <p>The meetup description</p>
-    </Fragment>
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/5/58/Milan.Proper.Wikipedia.Image.png"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a First meetup"
+    />
   );
 }
 
